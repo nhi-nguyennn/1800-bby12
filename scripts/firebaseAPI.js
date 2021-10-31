469c11263c6a2e6dd14a6b9d17717f9eedd7fc3f
mingli6809 <90582220+mingli6809@users.noreply.github.com>
2021-10-30 21:30:50 -0700

    first

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var  firebaseConfig = {
+  apiKey: "AIzaSyAZaU-JJ3zDwj2UFtwkB8u9yIiwENsdBkk",
+  authDomain: "comp-1800-ming.firebaseapp.com",
+  projectId: "comp-1800-ming",
+  storageBucket: "comp-1800-ming.appspot.com",
+  messagingSenderId: "633646453407",
+  appId: "1:633646453407:web:a1d42bc131af469dc388aa"
+  };
+  
+
+  const app = firebase.initializeApp(firebaseConfig);
+  
